47d36953bf3ec09e33783b53057af27ea6b5c3e6
original <imazekikeita@originalnonotobukkukonpyuta.local>
2025-07-11 16:24:42 +0900

    passwordreset#1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,15 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_mailer.default_url_options = { host: 'order.onrender.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "order-27ds.onrender.com",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
