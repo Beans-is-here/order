13de9c789dd7feb3360ecd1dfa56cacee4915188
original <imazekikeita@originalnonotobukkukonpyuta.local>
2025-09-09 23:20:12 +0900

    Rspec#6

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -43,28 +43,32 @@ RSpec.describe "Users", type: :system do
     end
   end
 
-#  describe 'topページからindexページに遷移する' do
-#    context 'ログインしている場合' do
-#      before do
-#        visit new_user_session_path
-#        fill_in 'メールアドレス', with: user.email
-#        fill_in 'パスワード', with: user.password
-#        click_button 'ログインする'
-#        expect(page).to have_content('ログインしました。')
-#      end
-    #it 'ログインしていないなら登録画面' do
-    #  visit root_path
-    #  click_link '記録をはじめる'
-    #  expect(current_path).to eq new_user_registration_path
-    #end
+  describe 'topページからindexページに遷移する' do
+    context 'ログインしている場合' do
+      before do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: user.password
+        click_button 'ログインする'
+        expect(page).to have_content('ログインしました。')
+      end
 
-#      it '投稿一覧に遷移する' do
-#        visit root_path
-        # デバッグ用：ページの内容を確認
-#        expect(page).to have_link('記録をはじめる')
-#        click_link '記録をはじめる'
-#        expect(current_path).to eq orders_path
-#      end
-#    end
-#  end
+      it '投稿一覧に遷移する' do
+        visit root_path
+        expect(page).to have_link('記録をはじめる')
+  
+      # JavaScript実行による遷移（Turboを回避）
+        page.execute_script("window.location.href = '#{orders_path}'")  
+        expect(current_path).to eq orders_path
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログインしていないなら登録画面' do
+        visit root_path
+        page.execute_script("window.location.href = '#{new_user_registration_path}'")
+        expect(current_path).to eq new_user_registration_path
+      end
+    end
+  end
 end
