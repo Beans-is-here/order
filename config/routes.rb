c08038eb20c6b51de73d9b6529ce3cc9b5ab4049
original <imazekikeita@originalnonotobukkukonpyuta.local>
2025-09-22 11:05:31 +0900

    パスワードリセット画面レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
