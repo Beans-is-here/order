974022200a9d24cb1a62149ebb1a779599bf41d5
original <imazekikeita@originalnonotobukkukonpyuta.local>
2025-11-09 12:59:04 +0900

    管理画面_role追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_05_094152) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_09_033743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_05_094152) do
     t.string "provider"
     t.string "uid"
     t.string "username"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
