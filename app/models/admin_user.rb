90a298f7402b761541841095ee19bf23ff0454bf
original <imazekikeita@originalnonotobukkukonpyuta.local>
2025-12-13 17:38:58 +0900

    管理画面_fix#9

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,13 +1,13 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-#  devise :database_authenticatable,
-#         :recoverable, :rememberable, :validatable
-#  def self.ransackable_attributes(_auth_object = nil)
-#    %w[id email created_at updated_at]
-#  end
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id email created_at updated_at]
+  end
 
-#  def self.ransackable_associations(_auth_object = nil)
-#    []
-#  end
+  def self.ransackable_associations(_auth_object = nil)
+    []
+  end
 end
